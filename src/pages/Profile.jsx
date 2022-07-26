e5ab784fd848b8e8d6510aadb4422e3462452ad0
Guilherme Leao <leaob7@gmail.com>
2022-07-26 14:53:56 -0300

    refactor: Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 import '../styles/Profile/profile.css';
 import UserLogo from '../utils/profileLogo';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setUsername();
+  }
+
+  setUsername = async () => {
+    const { name } = await getUser();
+    this.setState({ username: name });
+  }
+
   render() {
+    const { username } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
@@ -17,9 +35,23 @@ class Profile extends React.Component {
             </button>
           </div>
 
-          <h4>Nome</h4>
-          <h4>Email: </h4>
-          <h4>Descrição: </h4>
+          <h4>{ username }</h4>
+          <h4>{ `Email: ${username}@email.com` }</h4>
+
+          <div className="description">
+            <h4>
+              Descrição:
+            </h4>
+            <p>
+              {`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Sed mattis commodo tortor,
+              ac mattis tortor tristique in.
+              Donec efficitur pellentesque lectus, ut dignissim nisl.
+              Suspendisse potenti.
+              Integer euismod porta massa sed mattis. Suspendisse sagittis diam est.`}
+            </p>
+          </div>
+
         </section>
       </div>
     );
