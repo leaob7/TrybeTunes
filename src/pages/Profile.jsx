393a9b077106c98fff40aa652c321190c75d34cb
Guilherme Leao <leaob7@gmail.com>
2022-07-27 18:30:14 -0300

    refactor: Profile and ProfileEdit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-closing-tag-location */
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
@@ -19,12 +20,12 @@ class Profile extends React.Component {
   }
 
   setUsername = async () => {
-    const { name } = await getUser();
-    this.setState({ username: name });
+    const { name, email, description } = await getUser();
+    this.setState({ username: name, email, description });
   }
 
   render() {
-    const { username, editClicked } = this.state;
+    const { username, email, description, editClicked } = this.state;
 
     if (editClicked) return <Redirect to="/profile/edit" />;
 
@@ -41,20 +42,24 @@ class Profile extends React.Component {
           </div>
 
           <h4>{ username }</h4>
-          <h4>{ `Email: ${username}@email.com` }</h4>
+
+          { email ? <h4>{ `Email: ${email}` }</h4>
+            : <h4>{ `Email: ${username}@email.com` }</h4>}
 
           <div className="description">
             <h4>
               Descrição:
             </h4>
-            <p>
-              {`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            { description
+              ? <h4>{description}</h4>
+              : <p>
+                {`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Sed mattis commodo tortor,
               ac mattis tortor tristique in.
               Donec efficitur pellentesque lectus, ut dignissim nisl.
               Suspendisse potenti.
               Integer euismod porta massa sed mattis. Suspendisse sagittis diam est.`}
-            </p>
+              </p> }
           </div>
 
         </section>
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,32 +1,75 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import '../styles/ProfileEdit/profile-edit.css';
+import { createUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+      submit: false,
+    };
+  }
+
+  handleUserValues = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = async () => {
+    const { name, email, image, description } = this.state;
+    await createUser({ name, email, image, description });
+    this.setState({ submit: true });
+  }
+
   render() {
+    const { submit } = this.state;
+    if (submit) return <Redirect to="/profile" />;
+
     return (
       <div data-testid="page-profile-edit">
         <Header />
         <section>
 
-          <form action="" className="edit-user-form">
+          <form className="edit-user-form">
 
             <label htmlFor="input-name">
               <p>Nome</p>
-              <input type="text" id="input-name" placeholder="Adicione um nome" />
+              <input
+                type="text"
+                id="input-name"
+                name="name"
+                placeholder="Adicione um nome"
+                onChange={ this.handleUserValues }
+              />
             </label>
 
             <label htmlFor="input-email">
               <p>Email</p>
-              <input type="text" id="input-email" placeholder="Adicione um email" />
+              <input
+                type="text"
+                id="input-email"
+                name="email"
+                placeholder="Adicione um email"
+                onChange={ this.handleUserValues }
+              />
             </label>
 
             <label htmlFor="description-area">
               <p>Descrição</p>
-              <textarea id="description-area" placeholder="Adicione uma descrição " />
+              <textarea
+                id="description-area"
+                placeholder="Adicione uma descrição"
+                name="description"
+                onChange={ this.handleUserValues }
+              />
             </label>
 
-            <button type="button">
+            <button type="button" onClick={ this.handleSubmit }>
               Salvar
             </button>
 
